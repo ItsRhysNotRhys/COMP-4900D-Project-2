0a60daac4edbc6727536b07b1eea3b0b8857a110
ItsRhysNotRhys <80861613+ItsRhysNotRhys@users.noreply.github.com>
2025-03-10 16:01:27 -0400

    Splash on collision with obstacles

--- Assets/Obstacles/obstacle.gd
+++ Assets/Obstacles/obstacle.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+var Splash = preload("res://Player/splash.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,5 +13,7 @@ func _process(_delta: float) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Projectile"):
+		var effect = Splash.instantiate()
+		effect.position = area.position
+		area.get_parent().add_child(effect)
 		area.queue_free()
-	
