7db78820a26330518c7b58caca4af2e3095cbbd4
rightdress <belwang@cmail.carleton.ca>
2025-03-11 12:38:51 -0400

    added lore screen and credits, and added some more obstacles to linear levels

--- Levels/continue_button.gd
+++ Levels/continue_button.gd
@@ -1,6 +1,7 @@
 extends Button
 
-@onready var next_scene_path := "res://Levels/lore_screen.tscn"
+@onready var next_scene_path := "res://Levels/level_1.tscn"
 
 func _on_pressed() -> void:
+	print("pressed")
 	get_tree().change_scene_to_file(next_scene_path)
